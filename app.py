915c3b5d2ad877d63a94594b808eb74005835ab1
clam <cindyberri@gmail.com>
2024-12-16 17:56:38 -0500

    added filter by genre button/function, some css button updates

--- app.py
+++ app.py
@@ -531,10 +531,58 @@ def show_spotify_info():
     for playlist in playlists:
         playlist_dict[playlist.playlist_name] = [song.song_name for song_by_playlist, song in songs_by_playlist if song_by_playlist.playlist_id == playlist.playlist_id]
 
-    #print(playlist_dict)
     return render_template("spotify_info.html", ps=playlists, sg = playlist_dict)
-    
 
+@app.route("/spotify-info/genre")
+def show_spotify_info_by_genre():
+    # if somehow didn't grab user token, redirect to login
+    if not oauth_manager.validate_token(oauth_manager.get_cached_token()):
+        return redirect("/")
+
+    # set our session info
+    session['token_info'], authorized = get_token(session)
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+
+    # setup spotify object so we can get our data
+    sp = Spotify(auth=session.get('token_info').get('access_token'))
+    
+    user_id = sp.me()["id"]
+
+   # Query all playlists from the current user
+    playlists = Playlists.query.filter_by(user_id=user_id).all()
+    playlist_ids = [playlist.playlist_id for playlist in playlists]
+
+    # Join with SongByPlaylist where playlist ID matches
+    songs_by_playlist = db.session.query(SongByPlaylist, Song, Artist).join(
+        Song, Song.song_id == SongByPlaylist.song_id).join(
+        SongByArtist, Song.song_id == SongByArtist.song_id).join(
+        Artist, Artist.artist_id == SongByArtist.artist_id).filter(
+        SongByPlaylist.playlist_id.in_(playlist_ids)).all()
+
+    songs_by_playlists_ids = [song_by_playlist.song_id for song_by_playlist, song, artist in songs_by_playlist]
+
+    # Get genre info by connecting with Artist and ArtistByGenre
+    songs_by_genre = db.session.query(Song, Artist, Genre).join(
+        SongByArtist, Song.song_id == SongByArtist.song_id).join(
+        Artist, Artist.artist_id == SongByArtist.artist_id).join(
+        ArtistByGenre, Artist.artist_id == ArtistByGenre.artist_id).join(
+        Genre, Genre.genre_id == ArtistByGenre.genre_id).filter(
+        Song.song_id.in_(songs_by_playlists_ids)).all()
+
+    # Create a dictionary with genre names as keys and lists of song names as values
+    genre_dict = {}
+    for song, artist, genre in songs_by_genre:
+        if genre.genre_name not in genre_dict:
+            genre_dict[genre.genre_name] = []
+        genre_dict[genre.genre_name].append(song.song_name)
+    
+    #for genre in list(genre_dict.keys()):
+    #    print(genre, ": ", genre_dict[genre])
+
+
+    return render_template("spotify_genre_info.html", gn=list(genre_dict.keys()), sg=genre_dict)
 
 
 
