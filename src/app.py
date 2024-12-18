04a5291a86c2a5aa6c146731d42576b5fad73372
lschleck24 <luke.schleck@gmail.com>
2024-12-18 10:34:31 -0500

    made new models.py file

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 # run with
-# flask --app app run --debug --port 5000
+# flask --app app run --debug --port 3000
 
 import os
 import time
@@ -23,6 +23,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from spotipy import Spotify, CacheHandler
 from spotipy.oauth2 import SpotifyOAuth
 from sqlalchemy import select
+from src.models import db, User, Playlists, SongByPlaylist, Song, Artist, SongByArtist, Genre, ArtistByGenre
 
 class CacheSessionHandler(CacheHandler):
     def __init__(self, session, token_key):
@@ -46,160 +47,9 @@ app.secret_key = "DEV"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 
-# database stuff
-
-    # to set up database, do:
-    #   from app import app, db, User, Playlists, SongByPlaylist, Song, Artist, SongByArtist, Genre, ArtistByGenre
-    #   db.create_all()
-    # to clear database, do:
-    # models.[TABLE TO CLEAR].query.delete()
-
-db = SQLAlchemy(app)
+db.init_app(app)
 app.app_context().push()
 
-# models
-class User(db.Model):
-    user_table_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String)
-
-    db.UniqueConstraint(user_id)
-
-
-    def __init__(self, user_id):
-        self.user_id = user_id
-
-    def __repr__(self):
-        return f"User: ('{self.user_id}')"
-
-class Playlists(db.Model):
-    playlist_table_id = db.Column(db.Integer, primary_key=True)
-    playlist_id = db.Column(db.String)
-    playlist_name = db.Column(db.String)
-    user_id = db.Column(db.String, db.ForeignKey('user.user_id'), nullable=False)
-    snapshot_id = db.Column(db.String)
-
-    db.UniqueConstraint(playlist_id)
-
-
-    def __init__(self, playlist_id, playlist_name, user_id, snapshot_id):
-        self.playlist_id = playlist_id
-        self.playlist_name = playlist_name
-        self.user_id = user_id
-        self.snapshot_id = snapshot_id
-
-    def __repr__(self):
-        return f"Playlist: ('{self.playlist_id}', '{self.playlist_name}', '{self.snapshot_id}', '{self.user_id}')"
-
-class SongByPlaylist(db.Model):
-    song_table_id = db.Column(db.Integer(), primary_key=True)
-    song_id = db.Column(db.String)
-    playlist_id = db.Column(db.String, nullable=False) 
-
-    db.UniqueConstraint(song_id, playlist_id)
-
-
-    def __init__(self, song_id, playlist_id):
-        self.song_id = song_id
-        self.playlist_id = playlist_id
-
-    def __repr__(self):
-        return f"Song: ('{self.song_id}', '{self.playlist_id}')"
-
-class Song(db.Model):
-    song_table_id = db.Column(db.Integer(), primary_key=True)
-    song_id = db.Column(db.String, nullable=False)
-    song_name = db.Column(db.String, nullable=False)
-    year = db.Column(db.Integer(), nullable=False)
-    month = db.Column(db.Integer(), nullable=False)
-    day = db.Column(db.Integer(), nullable=False)
-
-    db.UniqueConstraint(song_id)
-
-    def __init__(self, song_id, song_name, year, month, day):
-        self.song_id = song_id
-        self.song_name = song_name
-        self.year = year
-        self.month = month
-        self.day = day
-
-    def __repr__(self):
-        return f"Song: ('{self.song_id}', '{self.song_name}')"
-
-class Artist(db.Model):
-    artist_table_id = db.Column(db.Integer(), primary_key=True)
-    artist_id = db.Column(db.String(50), nullable=False)
-    artist_name = db.Column(db.String(100), nullable=False)
-
-    db.UniqueConstraint(artist_id)
-
-    def __init__(self, artist_id, artist_name):
-        self.artist_id = artist_id
-        self.artist_name = artist_name
-
-    def __repr__(self):
-        return f"Song: ('{self.artist_id}', '{self.artist_name}')"
-
-
-class SongByArtist(db.Model):
-    artist_song_table_id = db.Column(db.Integer, primary_key = True)
-    artist_id = db.Column(db.String,db.ForeignKey('song.song_id'),nullable=False)
-    song_id = db.Column(db.String,db.ForeignKey('artist.artist_id'),nullable=False)
-    
-    db.UniqueConstraint(artist_id, song_id)
-
-    def __init__(self, artist_id, song_id):
-        self.artist_id = artist_id
-        self.song_id = song_id
-
-    def __repr__(self):
-        return f"SongByArtist: ('{self.artist_id}','{self.genre_id}')"
-    
-
-class Genre(db.Model):
-    genre_id = db.Column(db.Integer(), primary_key=True)
-    genre_name = db.Column(db.String)
-
-    db.UniqueConstraint(genre_name)
-
-    def __init__(self,genre_name):
-        self.genre_name = genre_name
-
-    def __repr__(self):
-        return f"Genre: ('{self.genre_name}')"
-
-
-class ArtistByGenre(db.Model):
-    artist_genre_table_id = db.Column(db.Integer, primary_key = True)
-    artist_id = db.Column(db.String,db.ForeignKey('artist.artist_id'),nullable=False)
-    genre_id = db.Column(db.String,db.ForeignKey('genre.genre_id'),nullable=False)
-
-    db.UniqueConstraint(artist_id, genre_id)
-
-    def __init__(self, artist_id, genre_id):
-        self.artist_id = artist_id
-        self.genre_id = genre_id
-
-    def __repr__(self):
-        return f"ArtistByGenre: ('{self.artist_id}','{self.genre_id}')"
-
-
-# BELOW IS DEPRECATED; USE ARTISTBYGENRE INSTEAD
-# class SongByGenre(db.Model):
-#     song_genre_table_id = db.Column(db.Integer, primary_key = True)
-#     song_id = db.Column(db.String,db.ForeignKey('song.song_id'),nullable=False)
-#     genre_id = db.Column(db.String,db.ForeignKey('genre.genre_id'),nullable=False)
-
-#     def __init__(self, id, song_id, genre_id):
-#         self.song_id = song_id
-#         self.genre_id = genre_id
-
-#     def __repr__(self):
-#         return f"SongByGenre: ('{self.song_id}','{self.genre_id}')"
-
-
-# TODO: add Genre, SongByArtist, SongByGenre
-# https://www.geeksforgeeks.org/connect-flask-to-a-database-with-flask-sqlalchemy/#setting-up-sqlalchemy
-# https://flask-sqlalchemy.readthedocs.io/en/stable/quickstart/#installation
 
 
 # setup spotify stuff
@@ -224,13 +74,6 @@ oauth_manager = SpotifyOAuth(
 @app.route("/")
 def homepage():
     jinja_env = {}
-
-    # i will look at this later but i think i alr handled this in the callback
-    # if request.args.get("code") or oauth_manager.validate_token(
-    #     oauth_manager.get_cached_token()
-    # ):
-    #     oauth_manager.get_access_token(request.args.get("code"))
-    #     return redirect("/spotify-info")
 
     return render_template(
         "index.html", spotify_auth_url=oauth_manager.get_authorize_url()
